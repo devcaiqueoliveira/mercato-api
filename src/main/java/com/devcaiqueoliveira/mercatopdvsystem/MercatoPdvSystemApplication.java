4cc223fc844845ec5313821eefdecfe06729204d
Caique Oliveira Santos <devcaiqueoliveira@gmail.com>
2025-11-24 17:15:51 -0300

    Initial commit: Setup do projeto com Docker e Flyway

---
+++ src/main/java/com/devcaiqueoliveira/mercatopdvsystem/MercatoPdvSystemApplication.java
@@ -0,0 +1,13 @@
+package com.devcaiqueoliveira.mercatopdvsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercatoPdvSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercatoPdvSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devcaiqueoliveira/mercatopdvsystem/MercatoPdvSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcaiqueoliveira.mercatopdvsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercatoPdvSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
