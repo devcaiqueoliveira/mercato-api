4fae4c054e3e05bfa8a15122f578dba569977ece
Caique Oliveira Santos <devcaiqueoliveira@gmail.com>
2025-11-25 20:14:34 -0300

    feat: adiciona classe Controller para leitura de requisições da entidade Category

---
+++ src/main/java/com/devcaiqueoliveira/mercatopdvsystem/controller/CategoryController.java
@@ -0,0 +1,41 @@
+package com.devcaiqueoliveira.mercatopdvsystem.controller;
+
+import com.devcaiqueoliveira.mercatopdvsystem.entity.Category;
+import com.devcaiqueoliveira.mercatopdvsystem.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@AllArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.listAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.save(category));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteCategoryById(@PathVariable Long id) {
+        categoryService.deleteById(id);
+        return ResponseEntity.notFound().build();
+    }
+
+
+}
