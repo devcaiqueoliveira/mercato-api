8caa40677e20cd001d3ba40c92d4ff16ef143ee2
Caique <devcaiqueoliveira@gmail.com>
2025-11-26 13:32:31 -0300

    feat: cria classe global que cuida das exceções da api | adiciona exceção para entidades não encontradas

---
+++ src/main/java/com/devcaiqueoliveira/mercatopdvsystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.devcaiqueoliveira.mercatopdvsystem.exception;
+
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> handleEntityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        StandardError err = new StandardError(
+                Instant.now(),
+                status.value(),
+                "Recurso não encontrado.",
+                e.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(err);
+    }
+}
