3ac95d3ea4ae5e22729f15670279cf4c652c490f
Caique <devcaiqueoliveira@gmail.com>
2025-12-23 10:26:11 -0300

    feat: adiciona entidade de item numa venda

--- src/main/java/com/devcaiqueoliveira/mercatopdvsystem/entity/Sale.java
+++ src/main/java/com/devcaiqueoliveira/mercatopdvsystem/entity/Sale.java
@@ -1,6 +1,10 @@
 package com.devcaiqueoliveira.mercatopdvsystem.entity;
 
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -8,17 +12,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "sales")
 public class Sale {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true, length = 10, updatable = false)
     private String code;
 
+    @Column(name = "total_amount", precision = 19, scale = 2)
     private BigDecimal totalAmount = BigDecimal.ZERO;
 
+    @OneToMany(mappedBy = "sale", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<SaleItem> items = new ArrayList<>();
 
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @PrePersist
     public void generateCode() {
