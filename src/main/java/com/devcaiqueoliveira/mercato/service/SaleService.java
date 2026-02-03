874cf4e314bf78104bcde46e4787d4a508ae4446
Caique <devcaiqueoliveira@gmail.com>
2026-02-03 10:30:28 -0300

    feat: adiciona classe de serviço para entidade Sale com metodo de buscar e criação de Sale

--- src/main/java/com/devcaiqueoliveira/mercato/service/SaleService.java
+++ src/main/java/com/devcaiqueoliveira/mercato/service/SaleService.java
@@ -1,5 +1,49 @@
 package com.devcaiqueoliveira.mercato.service;
 
+import com.devcaiqueoliveira.mercato.entity.Product;
+import com.devcaiqueoliveira.mercato.entity.Sale;
+import com.devcaiqueoliveira.mercato.entity.SaleItem;
+import com.devcaiqueoliveira.mercato.exception.EntityNotFoundException;
+import com.devcaiqueoliveira.mercato.repository.ProductRepository;
+import com.devcaiqueoliveira.mercato.repository.SaleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SaleService {
+
+    private final SaleRepository saleRepository;
+    private final ProductRepository productRepository;
+
+    public Sale create(Sale saleInput) {
+
+        List<SaleItem> rawItems = new ArrayList<>(saleInput.getItems());
+        saleInput.getItems().clear();
+
+        for (SaleItem rawItem : rawItems) {
+
+            Long productId = rawItem.getProduct().getId();
+
+            Product realProduct = productRepository.findById(productId)
+                    .orElseThrow(() -> new EntityNotFoundException("Produto não encontrado ID: " + productId));
+
+            realProduct.decreaseStock(rawItem.getQuantity());
+
+            saleInput.addItem(realProduct, rawItem.getQuantity());
+        }
+
+        saleInput.finalizeSale();
+
+        return saleRepository.save(saleInput);
+    }
+
+    public Sale findById(Long id) {
+        return saleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Venda com ID: " + id + " não encontrada."));
+    }
 
 }
